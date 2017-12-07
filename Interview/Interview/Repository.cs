d3be4b289d1c772ef71205c98a2d8538a5de0129
bookingmanager18 <bookingmanager18@gmail.com>
2017-12-07 23:30:21 +0000

    Add placeholder for repository interface implementation.

---
+++ Interview/Interview/Repository.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace Interview
+{
+    public class Repository<T> : IRepository<T> where T : IStoreable
+    {
+        private HashSet<T> list;
+
+        public Repository()
+        {
+            list = new HashSet<T>();
+        }
+
+        public IEnumerable<T> All()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete(System.IComparable id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T FindById(System.IComparable id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Save(T item)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
